34396b023f6ff17644a2c09950a473fb8b1974fc
baobaozi <work@mingteo.com>
2025-06-09 16:42:13 +1000

    light-dark inside SVG doesn't work on mobile, so use separate images/classes

--- src/components/at/site-heading-cta/index.tsx
+++ src/components/at/site-heading-cta/index.tsx
@@ -1,12 +1,14 @@
 import mingLogo from "../../../assets/ming-logo.svg";
+import mingLogoLight from "../../../assets/ming-logo-light.svg";
 import style from "./style.module.css";
 
-const { siteHeadingCtaSt, logoSt } = style;
+const { siteHeadingCtaSt, logoSt, logoDarkSt, logoLightSt } = style;
 
 export default function AtSiteHeadingCta(): React.ReactNode {
     return (
-        <a href="/" className={siteHeadingCtaSt}>
-            <img src={mingLogo} className={logoSt} alt="Ming Teo" />
+        <a href="/" className={siteHeadingCtaSt} aria-label="Ming Teo">
+            <img src={mingLogo} className={`${logoSt} ${logoDarkSt}`} alt="" />
+            <img src={mingLogoLight} className={`${logoSt} ${logoLightSt}`} alt="" />
         </a>
     );
 }

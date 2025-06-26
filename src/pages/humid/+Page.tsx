ce3670c5b7a8f0e978a49fc32c15974e271933ab
baobaozi <work@mingteo.com>
2025-06-26 16:14:19 +1000

    create humidity page for showcasing fetching from API

---
+++ src/pages/humid/+Page.tsx
@@ -0,0 +1,12 @@
+import PageHeading from "../../components/at/page-heading";
+import AtShoutbox from "../../components/at/shoutbox";
+
+export default function HumidPage(): React.ReactNode {
+    return (
+        <>
+            <PageHeading>How humid is it?</PageHeading>
+            <p>Sample code of API fetching</p>
+            <AtShoutbox>work (at) mingteo (dot) com</AtShoutbox>
+        </>
+    );
+}
